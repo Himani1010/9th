4f2be0fb610e8c7ef0b07ff1755a86b770b9b506
Himani <84378418+Himani1010@users.noreply.github.com>
2021-12-14 10:08:14 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ var books = {
       rating: "5/5"
     },
     {
+      name: "Eloquent JavaScript",
+      rating: "4/5"
+    },
+    {
       name: "Clean Code in JavaScript ",
       rating: "3.5/5"
     }
@@ -18,6 +22,11 @@ var books = {
       name: "The Girl Who Drank the Moon ",
       rating: "4/5"
     },
+
+    {
+      name: "Shiva Trilogy",
+      rating: "1/5"
+    },
     {
       name: " The War of Art",
       rating: "1/5"
@@ -27,6 +36,10 @@ var books = {
     {
       name: "Think and Grow Rich ",
       rating: "5/5"
+    },
+    {
+      name: "Loonshots",
+      rating: "3.5/6"
     },
     {
       name: "The Tatas",
